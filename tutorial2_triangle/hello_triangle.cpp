b57b5972db53117b7100bb24df3dc01a9e6e07ff
datvo06@gmail.com <datvo06@gmail.com>
2018-01-05 18:31:05 +0700

    Added vertex shader and fragment shader

---
+++ tutorial2_triangle/hello_triangle.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <Dat/string_utils.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void init();
+void render();
+void terminate();
+
+
+GLfloat vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+GLuint VBO;
+GLuint vertexShader;
+GLuint fragmentShader;
+GLuint shaderProgram;
+
+
+int main ()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //On Mac only
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	// Call this before using any openGL function
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	init();
+	while(!glfwWindowShouldClose(window)){
+		// Input
+		processInput(window);
+		// Rendering
+		// Check poll events and swap buffer
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	terminate();
+	return 0;
+}
+
+
+void init(){
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glGenBuffers(1, &VBO);
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	const char* shaderSource = DatTools::Util::string_from_file("vertex.glsl").c_str();
+	glShaderSource(vertexShader, 1, &shaderSource, NULL);
+	glCompileShader(vertexShader);
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success){
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	const char* fragmentShaderSource = DatTools::Util::string_from_file("fragment.glsl").c_str();
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success){
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success){
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+	}
+	/*
+	 * Linked to the program, no longer needed
+	 */
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+	glViewport(0, 0, width, height);
+}
+
+
+void processInput(GLFWwindow* window){
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+void render(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	// 0. Setup buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 1. Vertex Attribute pointer
+	//location in vertex shader, vertex size, vertex type, normalize to 0-1?, stride - space
+	//between consecutive vertex attribute set, (void*) offset
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	// 2. Use program
+	glUseProgram(shaderProgram);
+	// 3. draw
+}
+
+
+void terminate(){
+	glfwTerminate();
+}
