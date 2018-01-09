a6b070c8506972e9261e700fb702c364f390ebc0
datvo06@gmail.com <datvo06@gmail.com>
2018-01-09 11:03:05 +0700

    Done walking

---
+++ tutorial7_1_userInputCamera/fragment.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 texCoord;
+
+
+uniform sampler2D wallTexture;
+uniform sampler2D faceTexture;
+
+void main()
+{
+	vec4 wallColor = texture(wallTexture, texCoord);
+	vec4 faceColor = texture(faceTexture, texCoord);
+	FragColor = mix(wallColor, faceColor, 0.2);//*vec4(ourColor, 1.0f);
+}
